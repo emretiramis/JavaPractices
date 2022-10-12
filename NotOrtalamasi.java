3525920846d28fd3a32dc9c273a86a53d39deb37
Emre TIRAMIŞ <112012337+emretiramis@users.noreply.github.com>
2022-10-12 23:15:22 +0300

    Add files via upload

---
+++ NotOrtalamasi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,tarih,turkce;
+        Scanner abc= new Scanner(System.in);
+        System.out.println("Lütfen matematik notunuzu giriniz : ");
+        mat= abc.nextInt();
+        System.out.println("Lütfen fizik notunuzu giriniz : ");
+        fizik=abc.nextInt();
+        System.out.println("Lütfen kimya notunuzu giriniz : ");
+        kimya=abc.nextInt();
+        System.out.println("Lütfen tarih notunuzu giriniz : ");
+        tarih=abc.nextInt();
+        System.out.println("Lütfen turkce notunuzu giriniz : ");
+        turkce=abc.nextInt();
+        int toplam=mat+fizik+kimya+tarih+turkce;
+        double sonuc=toplam/5;
+        System.out.println("Ortalamanız : "+sonuc);
+    }
+}
